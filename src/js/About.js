700a710bc74a56704bfa4ce0679cd5717383ed29
redvus <a.suvorof@gmail.com>
2025-02-07 00:11:32 +0400

    Начал подправлять About под новые стандарты.

--- src/js/About.js
+++ src/js/About.js
@@ -3,42 +3,28 @@ import { gsap } from "gsap";
 class About {
 
     constructor() {
-        this.aboutBack();
+        this.initLayout();
+        this.aboutAnim();
+        this.aboutAuthorsAnim();
     }
 
-    aboutBack() {
-        const
-            wrapperBack = document.querySelector('.wrapper__back'),
-            wrapperAboutBack = document.createElement('div')
-        ;
+    initLayout() {
+        this.container = document.querySelector('.container');
+        this.wrapperTitleAuthors = document.createElement('div');
+        this.containerAbout = document.createElement('div');
+        this.wrapperTop = document.querySelector('.wrapper__top');
 
-        wrapperAboutBack.className = 'wrapper__intro wrapper__intro_about';
-        wrapperBack.appendChild(wrapperAboutBack);
+        this.wrapperBack = document.querySelector('.wrapper__back');
+        this.wrapperAboutBack = document.createElement('div');
+        this.wrapperBackBlock = document.querySelector('.wrapper__back');
 
-        const wrapperBackBlock = document.querySelector('.wrapper__back');
+        this.wrapperAboutBack.className = 'wrapper__intro wrapper__intro_about';
+        this.wrapperBack.appendChild(this.wrapperAboutBack);
 
-        function aboutAnim() {
-            let tl = gsap.timeline();
-            tl
-                // .from(wrapperTitle, {
-                //     duration: 0.4,
-                //     autoAlpha: 0,
-                //     y: '-10%'
-                // })
-                .from(wrapperBackBlock, {
-                    duration: 0.4,
-                    autoAlpha: 0
-                })
-                // .from(wrapperBottomMenu, {
-                //     duration: 0.4,
-                //     delay: '-0.6',
-                //     stagger: '0.05',
-                //     autoAlpha: 0
-                // })
-
-            ;
-        }
-        aboutAnim();
+        this.titleAuthors = document.querySelector('.wrapper__top_title');
+        this.containerAboutPerson = document.querySelectorAll('.container__about_person');
+        this.aboutBlock = document.querySelector('.container__about_block');
+        this.containerAboutInside = document.querySelector('.container__about_inside');
     }
 
     aboutAuthors(personFirstTitle,
@@ -48,27 +34,23 @@ class About {
                  personSecondName,
                  personThirdTitle,
                  personThirdName) {
-        const
-            container = document.querySelector('.container'),
-            wrapperTitleAuthors = document.createElement('div'),
-            containerAbout = document.createElement('div'),
-            wrapperTop = document.querySelector('.wrapper__top')
-        ;
+
 
         if (document.body.clientWidth < 570 || screen.width < 570) {
             container.style.width = 'calc(100% - 2rem)';
             container.style.padding = '0 1rem';
         } else {
-            container.style.width = '45rem';
+            this.container.style.width = '45rem';
         }
-        containerAbout.className = 'container__about_block container__about_block--authors';
-        wrapperTitleAuthors.className = 'wrapper__top_title';
 
-        wrapperTitleAuthors.innerHTML = `
+        this.containerAbout.className = 'container__about_block container__about_block--authors';
+        this.wrapperTitleAuthors.className = 'wrapper__top_title';
+
+        this.wrapperTitleAuthors.innerHTML = `
             <h1>Авторы</h1>
         `;
 
-        containerAbout.innerHTML = `
+        this.containerAbout.innerHTML = `
             <div class="container__about_inside container__about_text container__about_text--center">
                 <div class="container__about_person">
                     <h4>${personFirstTitle}</h4>
@@ -84,44 +66,58 @@ class About {
                 </div>
             </div>
         `;
-        wrapperTop.appendChild(wrapperTitleAuthors);
-        container.appendChild(containerAbout);
+        this.wrapperTop.appendChild(this.wrapperTitleAuthors);
+        this.container.appendChild(this.containerAbout);
+    }
 
-        const titleAuthors = document.querySelector('.wrapper__top_title'),
-            containerAboutPerson = document.querySelectorAll('.container__about_person'),
-            aboutBlock = document.querySelector('.container__about_block'),
-            containerAboutInside = document.querySelector('.container__about_inside')
+    aboutAnim() {
+        let tl = gsap.timeline();
+        tl
+            // .from(wrapperTitle, {
+            //     duration: 0.4,
+            //     autoAlpha: 0,
+            //     y: '-10%'
+            // })
+            .from(this.wrapperBackBlock, {
+                duration: 0.4,
+                autoAlpha: 0
+            })
+        // .from(wrapperBottomMenu, {
+        //     duration: 0.4,
+        //     delay: '-0.6',
+        //     stagger: '0.05',
+        //     autoAlpha: 0
+        // })
+        ;
+    }
+
+    aboutAuthorsAnim() {
+        let tl = new gsap.timeline();
+
+        tl
+            .from(this.aboutBlock, {
+                autoAlpha: 0,
+                duration: 0.4,
+                scale: 0.97
+            })
+            .from(this.titleAuthors, {
+                duration: '0.3',
+                delay: '-0.1',
+                autoAlpha: 0,
+                y: '-10%'
+            })
+            .from(this.containerAboutPerson, {
+                autoAlpha: 0,
+                duration: 0.4,
+                delay: '-0.2',
+                y: "-0.5rem",
+                stagger: 0.07
+            })
         ;
 
         if (document.body.clientWidth < 570 || screen.width < 570) {
-            containerAboutInside.style.height = '100%';
-        }        
-
-        function aboutAuthorsAnim() {
-            let tl = new gsap.timeline();
-
-            tl
-                .from(aboutBlock, {
-                    autoAlpha: 0,
-                    duration: 0.4,
-                    scale: 0.97
-                })
-                .from(titleAuthors, {
-                    duration: '0.3',
-                    delay: '-0.1',
-                    autoAlpha: 0,
-                    y: '-10%'
-                })
-                .from(containerAboutPerson, {
-                    autoAlpha: 0,
-                    duration: 0.4,
-                    delay: '-0.2',
-                    y: "-0.5rem",
-                    stagger: 0.07
-                })
-            ;
+            this.containerAboutInside.style.height = '100%';
         }
-        aboutAuthorsAnim();
     }
 
     aboutLibrary(libraryLeftText, libraryRightText) {
